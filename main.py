d0c59cc7db3236cdd45f1206b72ebbe7e0bfe2b3
Enrique del Valle Perez <enrique.delvalle@agilethought.com>
2022-10-26 18:51:18 -0500

    path finished

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 from kivy.config import Config
 
 Config.set('graphics', "width", '900')
@@ -6,7 +8,7 @@ Config.set('graphics', 'height', 400)
 from kivy import platform
 from kivy.core.window import Window
 from kivy.app import App
-from kivy.graphics import Color, Line, Quad
+from kivy.graphics import Color, Line, Quad, Triangle
 from kivy.properties import NumericProperty, Clock
 from kivy.uix.widget import Widget
 
@@ -17,8 +19,8 @@ class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
 
-    V_NB_LINES = 10
-    V_LINES_SPACING = .25  # percentage in screen width
+    V_NB_LINES = 8
+    V_LINES_SPACING = .2  # percentage in screen width
     vertical_lines = []
 
     H_NB_LINES = 15
@@ -33,9 +35,14 @@ class MainWidget(Widget):
     curren_speed_x = 0
     current_offset_x = 0
 
-    tile = None
-    ti_x = 0
-    ti_y = 0
+    NB_TILES = 16
+    tiles = []
+    tiles_coordinates = []
+
+    SHIP_WIDTH = .1
+    SHIP_HEIGHT = 0.035
+    SHIP_BASE_Y = 0.04
+    ship = None
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
@@ -43,6 +50,9 @@ class MainWidget(Widget):
         self.init_vertical_line()
         self.init_horizontal_line()
         self.init_tiles()
+        # self.update_ship()
+        self.pre_fill_tiles_coordinates()
+        self.generate_tile_coordinates()
 
         if self.is_desktop():
             self._keyboard = Window.request_keyboard(self.keyboard_closed, self)
@@ -59,10 +69,69 @@ class MainWidget(Widget):
         else:
             return False
 
+    def init_ship(self):
+        with self.canvas:
+            Color(0, 0, 0)
+            self.ship = Triangle()
+
+    def update_ship(self):
+        center_x = self.width / 2
+        base_y = self.SHIP_BASE_Y * self.height
+        ship_half_width = self.SHIP_WIDTH * self.width / 2
+        sehip_height = self.SHIP_HEIGHT * self.height
+
+        x1, y1 = self.transform(center_x - ship_half_width, base_y)
+        x2, y2 = self.transform(center_x, base_y + sehip_height)
+        x3, y3 = self.transform(center_x + ship_half_width, base_y)
+
+        self.ship.points = [x1, y1, x2, y2, x3, y3]
+
     def init_tiles(self):
         with self.canvas:
             Color(1, 1, 1)
-            self.tile = Quad()
+            for i in range(0, self.NB_TILES):
+                self.tiles.append(Quad())
+
+    def pre_fill_tiles_coordinates(self):
+        for i in range(0, 10):
+            self.tiles_coordinates.append((0, i))
+
+    def generate_tile_coordinates(self):
+        last_x = 0
+        last_y = 0;
+
+        for i in range(len(self.tiles_coordinates)-1, -1, -1):
+            if self.tiles_coordinates[i][1] < self.current_y_loop:
+                del self.tiles_coordinates[i]
+
+        if len(self.tiles_coordinates) > 0:
+            last_coordinates = self.tiles_coordinates[-1]
+            last_x = last_coordinates[0]
+            last_y = last_coordinates[1] + 1
+
+        for i in range(len(self.tiles_coordinates), self.NB_TILES):
+            r = random.randint(0, 2)
+
+            start_index = -int(self.V_NB_LINES / 2) + 1
+            end_index = start_index + self.V_NB_LINES - 1
+            if last_x <= start_index:
+                r = 1
+            if last_x >= end_index:
+                r = 2
+
+            self.tiles_coordinates.append((last_x, last_y))
+            if r == 1:
+                last_x += 1
+                self.tiles_coordinates.append((last_x, last_y))
+                last_y += 1
+                self.tiles_coordinates.append((last_x, last_y))
+            if r == 2:
+                last_x -= 1
+                self.tiles_coordinates.append((last_x, last_y))
+                last_y += 1
+                self.tiles_coordinates.append((last_x, last_y))
+
+            last_y += 1
 
     def init_vertical_line(self):
         with self.canvas:
@@ -70,6 +139,7 @@ class MainWidget(Widget):
             # self.line = Line(position=[100, 0, 100, 100])
             for i in range(0, self.V_NB_LINES):
                 self.vertical_lines.append(Line())
+
 
     def get_line_x_from_index(self, index):
         central_line_x = int(self.width / 2)
@@ -90,18 +160,21 @@ class MainWidget(Widget):
         return x, y
 
     def update_tiles(self):
-        xmin, ymin = self.get_tile_coordinates(self.ti_x, self.ti_y)
-        xmax, ymax = self.get_tile_coordinates(self.ti_x + 1, self.ti_y + 1)
+        for i in range(0, self.NB_TILES):
+            tile = self.tiles[i]
+            tile_coordinates = self.tiles_coordinates[i]
+            xmin, ymin = self.get_tile_coordinates(tile_coordinates[0], tile_coordinates[1])
+            xmax, ymax = self.get_tile_coordinates(tile_coordinates[0]+ 1, tile_coordinates[1] + 1)
 
-        # 2   3
-        # 1   4
+            # 2   3
+            #
+            # 1   4
+            x1, y1 = self.transform(xmin, ymin)
+            x2, y2 = self.transform(xmin, ymax)
+            x3, y3 = self.transform(xmax, ymax)
+            x4, y4 = self.transform(xmax, ymin)
 
-        x1, y1 = self.transform(xmin, ymin)
-        x2, y2 = self.transform(xmin, ymax)
-        x3, y3 = self.transform(xmax, ymax)
-        x4, y4 = self.transform(xmax, ymin)
-
-        self.tile.points = [x1, y1, x2, y2, x3, y3, x4, y4]
+            tile.points = [x1, y1, x2, y2, x3, y3, x4, y4]
 
     def update_vertical_lines(self):
         # -1 0 1 2
@@ -140,12 +213,14 @@ class MainWidget(Widget):
         self.update_vertical_lines()
         self.update_horizontal_lines()
         self.update_tiles()
+        # self.update_ship()
         self.current_offset_y += self.speed * time_factor
 
         spacing_y = self.H_LINES_SPACING * self.height
         if self.current_offset_y >= spacing_y:
             self.current_offset_y -= spacing_y
             self.current_y_loop += 1
+            self.generate_tile_coordinates()
 
         self.current_offset_x += self.curren_speed_x * time_factor
 
